efaf5bf71607605a26a0fef174fc95997b7073bd
romeo <romeoquist@gmail.com>
2024-02-29 17:13:14 -0600

    Created more practice problems with functions

--- src/FunWithFunctions.js
+++ src/FunWithFunctions.js
@@ -70,8 +70,31 @@ function calculateAreaOfImpossibleShape(length,dimensionCalculator){
 }
 
 // Calling an high-order function
+// The high-order function takes another function as a parameter !!!
 
 
 console.log(calculateAreaOfImpossibleShape(32,multiplyByTen));
+
+
+// Defining a one-use-function inside of a high order function
+
+
+let tools = ["drill", "saw", "hammer"];
+
+// defining a high order function first
+function printNumberedItems(items,format){
+
+    console.log(format(items));
+
+}
+
+printNumberedItems(tools, function(array){
+
+    let result = "";
+    for(let i = 0; i < array.length; i++){
+        result+= i+1 + ". " + array[i] + "\n";
+    }
+    return result;
+});
 
 
