e1237ccb6da5efdd2e5bef1ec6ac964b17976151
romeo <romeoquist@gmail.com>
2024-03-05 19:07:42 -0600

    added more practice to FunWithObjects

--- src/FunWithObjects.js
+++ src/FunWithObjects.js
@@ -1,4 +1,10 @@
 
+
+let burp1 = function(){
+
+}
+
+
 let person = {
 
     gender: "male",
@@ -6,9 +12,19 @@ let person = {
     weight: "176",
     eyeColor: "green",
     screamAtRandomPeople: function(){
-        console.log("HEEEELLLOOOOOO CITIZENNNNNNN")
-    }
+        //console.log("HEEEELLLOOOOOO CITIZENNNNNNN")
+    },
+    burp: burp1
+    
 };
+
+let cat = {
+
+    color: "black",
+    age: "1",
+    name: "Eva",
+
+}
 
 person.screamAtRandomPeople();
 
@@ -16,3 +32,27 @@ console.log(person.gender);
 console.log(person.height);
 console.log(person.weight);
 console.log(person.eyeColor);
+
+for(let key in cat){
+    console.log(key +": " + cat[key])
+}
+
+person.pet = cat;
+
+console.log("\n");
+console.log("\n");
+
+for(let key in person){
+
+    if(typeof(person[key]) === typeof(person.pet) || typeof(person[key]) === typeof(person.screamAtRandomPeople())){
+        console.log("!!!!!!!!!");
+    }else{
+        console.log(key + ": " + person[key] + "    type: " + typeof(person[key]));
+    }
+}
+
+console.log("\n");
+console.log("\n");
+
+console.log(typeof(person.pet))
+console.log(typeof(person.burp()))
