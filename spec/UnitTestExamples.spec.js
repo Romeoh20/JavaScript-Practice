26d92b7c2acdcf984ade6a6be81f06cb1ec67bcd
romeo <romeoquist@gmail.com>
2024-03-07 16:58:16 -0600

    Created data to be unit tested

---
+++ spec/UnitTestExamples.spec.js
@@ -0,0 +1,6 @@
+
+const addTwoNumbers = require("../src/UnitTestExamples.js");
+
+let numbersFunction = addTwoNumbers;
+
+console.log(numberFunction(2,2));
---
+++ src/UnitTestExamples.js
@@ -0,0 +1,7 @@
+
+
+function addTwoNumbers(num1,num2){
+    return num1 + num2;
+}
+
+module.export = addTwoNumbers;
