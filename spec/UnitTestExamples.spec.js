c59c39ed30aaf38ca08335e2109628f246f7589f
romeo <romeoquist@gmail.com>
2024-03-07 17:04:47 -0600

    created a unit test for for the unit test data that I created

--- spec/UnitTestExamples.spec.js
+++ spec/UnitTestExamples.spec.js
@@ -1,6 +1,10 @@
 
 const addTwoNumbers = require("../src/UnitTestExamples.js");
 
-let numbersFunction = addTwoNumbers;
 
-console.log(numberFunction(2,2));
+describe("The addTwoNums function", function(){
+    it("should return the sum of two numbers", function(){
+       expect(addTwoNumbers(2,2)).toEqual(4);
+    });
+
+});
