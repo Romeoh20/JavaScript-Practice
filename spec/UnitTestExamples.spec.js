5b7f8df3eaa3d249da18bc9c50d6326297eb9bae
romeo <romeoquist@gmail.com>
2024-03-22 15:13:49 -0500

    Jasmine works now and I have added my first unit test and it works

--- spec/UnitTestExamples.spec.js
+++ spec/UnitTestExamples.spec.js
@@ -1,10 +1,14 @@
 
 const addTwoNumbers = require("../src/UnitTestExamples.js");
 
+function addNumbers(num1,num2){
+    return num1 + num2
+}
 
-describe("The addTwoNums function", function(){
-    it("should return the sum of two numbers", function(){
-       expect(addTwoNumbers(2,2)).toEqual(4);
+
+describe("The addTwoNumber function", function() {
+    it("should return the sum of two numbers",function() {
+
+        expect(addNumbers(2,2)).toEqual(4)
     });
-
 });
